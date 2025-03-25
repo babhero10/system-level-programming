edf2a2b067aa7964b331ccf668ef9a0ee5b34c44
Abdullah Elsayed <abdallah.elsaid25@gmail.com>
2025-03-25 13:42:27 +0200

    ADD: pwd utility

---
+++ unix_utilities/pwd.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 200
+
+int main(void)
+{
+  char buf[BUFFER_SIZE];
+  char *res;
+
+  res = getcwd(buf, BUFFER_SIZE) ;
+
+  if (res == NULL)
+  {
+    if (errno == EACCES)
+    {
+      fprintf(stderr, "You don't have the premssion to access the directory!\n");
+      exit(-1);
+    }
+    else if (errno == ERANGE)
+    {
+      fprintf(stderr, "Ops!, Buffer reach it limit!\nThis problem can be solved later by reallocate the buffer with bigger size!\n"
+              "get_current_dir_name() can be used also but will require additional free!\n");
+      exit(-2);
+    }
+    else
+    {
+      fprintf(stderr, "Something went wrong!\n");
+      exit(-3);
+    }
+  }
+
+  printf("%s\n", res);
+
+  return 0;
+}
