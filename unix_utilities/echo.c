06d683144392ba575c61375413c877ba19bb1389
Abdullah Elsayed <abdallah.elsaid25@gmail.com>
2025-03-27 11:13:05 +0200

    ADD: echo utility

---
+++ unix_utilities/echo.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+void parse_string(char text[]);
+void parse_special_char(char special_char);
+
+int main(int argc, char *argv[]) {
+  int i;
+
+  for (i = 1; i < argc; i++) {
+    parse_string(argv[i]);
+    if (i < argc - 1){
+        printf(" ");
+    }
+  }
+
+  printf("\n");
+  return 0;
+}
+
+void parse_string(char text[]) {
+  const int len = strlen(text);
+  int i;
+
+  for (i = 0; i < len; i++) {
+    if (text[i] != '\\') {
+      fputc(text[i], stdout);
+    } else if (i + 1 < len) {
+      i++;
+      parse_special_char(text[i]);
+    }
+  }
+}
+
+void parse_special_char(char special_char) {
+    switch (special_char) {
+        case 'n': printf("\n"); break;
+        case 't': printf("\t"); break;
+        case 'r': printf("\r"); break;
+        case 'b': printf("\b"); break;
+        case 'f': printf("\f"); break;
+        case 'v': printf("\v"); break;
+        default: printf("%c", special_char); break;
+    }
+}
