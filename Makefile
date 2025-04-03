79ecfe85037fcb6dfa3c1e40d8132bf5b38cad30
Abdullah Elsayed <abdallah.elsaid25@gmail.com>
2025-04-03 14:35:24 +0200

    ADD: shells/femto

--- Makefile
+++ Makefile
@@ -3,29 +3,39 @@ CC = gcc
 CFLAGS = -Wall -Wextra -O2
 
 # Directories
-SRCDIR = unix_utilities
-BINDIR = executables
+UTILS_SRCDIR = unix_utilities
+SHELLS_SRCDIR = shells
+UTILS_BINDIR = executables/utilies
+SHELLS_BINDIR = executables/shells
 
-# List all .c files in the source directory
-SOURCES := $(wildcard $(SRCDIR)/*.c)
+# List all .c files in the utils and shells source directories
+UTILS_SOURCES := $(wildcard $(UTILS_SRCDIR)/*.c)
+SHELLS_SOURCES := $(wildcard $(SHELLS_SRCDIR)/*.c)
 
 # Create a list of targets by replacing the source directory and removing the .c extension
-TARGETS := $(patsubst $(SRCDIR)/%.c, $(BINDIR)/%, $(SOURCES))
+UTILS_TARGETS := $(patsubst $(UTILS_SRCDIR)/%.c, $(UTILS_BINDIR)/%, $(UTILS_SOURCES))
+SHELLS_TARGETS := $(patsubst $(SHELLS_SRCDIR)/%.c, $(SHELLS_BINDIR)/%, $(SHELLS_SOURCES))
 
-# Default target: build all executables
-all: $(TARGETS)
+# Default target: build all executables (both utils and shells)
+all: $(UTILS_TARGETS) $(SHELLS_TARGETS)
 
-# Rule to compile each .c file into an executable in the BINDIR
-$(BINDIR)/%: $(SRCDIR)/%.c | $(BINDIR)
+# Rule to compile each .c file into an executable in the appropriate BINDIR
+$(UTILS_BINDIR)/%: $(UTILS_SRCDIR)/%.c | $(UTILS_BINDIR)
 	$(CC) $(CFLAGS) -o $@ $<
 
-# Ensure the executables directory exists
-$(BINDIR):
-	mkdir -p $(BINDIR)
+$(SHELLS_BINDIR)/%: $(SHELLS_SRCDIR)/%.c | $(SHELLS_BINDIR)
+	$(CC) $(CFLAGS) -o $@ $<
+
+# Ensure the executables directories exist
+$(UTILS_BINDIR):
+	mkdir -p $(UTILS_BINDIR)
+
+$(SHELLS_BINDIR):
+	mkdir -p $(SHELLS_BINDIR)
 
 # Clean up generated executables
 clean:
-	rm -rf $(BINDIR)/*
+	rm -rf $(UTILS_BINDIR)/* $(SHELLS_BINDIR)/*
 
 .PHONY: all clean
 
---
+++ shells/femto_shell.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void get_args(int *argc, char **argv[], char *text);
+void echo(int argc, char *argv[]);
+
+int main(void) {
+
+  int my_argc;
+  char **my_argv = NULL;
+  char *buffer = NULL;
+  size_t len;
+  ssize_t read;
+
+  while (1) {
+    printf("Femto shell prompt > ");
+
+    read = getline(&buffer, &len, stdin);
+    if (read == -1) {
+      perror("Error occurred while reading input.\n");
+      free(buffer);
+      return 1;
+    }
+
+    buffer[read - 1] = '\0';
+
+    get_args(&my_argc, &my_argv, buffer);
+
+    if (my_argc > 0) {
+      if (strcmp(my_argv[0], "echo") == 0) {
+        echo(my_argc, my_argv);
+      } else if (strcmp(my_argv[0], "exit") == 0) {
+        printf("Good Bye!\n");
+        break;
+      } else if (strcmp(my_argv[0], "clear") == 0) {
+        printf("\033[H\033[J");
+      } else {
+        printf("Invalid command\n");
+      }
+    } else {
+      printf("\n");
+    }
+
+    free(my_argv);
+    my_argv = NULL;
+    free(buffer);
+    buffer = NULL;
+  }
+
+  return 0;
+}
+
+void get_args(int *argc, char ***argv, char *text) {
+  *argv = (char **)malloc((strlen(text) / 2) * sizeof(char *));
+  *argc = 0;
+
+  char *next_arg = strtok(text, " ");
+
+  while (next_arg != NULL) {
+    (*argv)[(*argc)++] = next_arg;
+    next_arg = strtok(NULL, " ");
+  }
+}
+
+void parse_special_char(char special_char) {
+  switch (special_char) {
+  case 'n':
+    printf("\n");
+    break;
+  case 't':
+    printf("\t");
+    break;
+  case 'r':
+    printf("\r");
+    break;
+  case 'b':
+    printf("\b");
+    break;
+  case 'f':
+    printf("\f");
+    break;
+  case 'v':
+    printf("\v");
+    break;
+  default:
+    printf("%c", special_char);
+    break;
+  }
+}
+
+void parse_string(char text[]) {
+  const int len = strlen(text);
+  int i;
+
+  for (i = 0; i < len; i++) {
+    if (text[i] != '\\') {
+      fputc(text[i], stdout);
+    } else if (i + 1 < len) {
+      i++;
+      parse_special_char(text[i]);
+    }
+  }
+}
+
+void echo(int argc, char *argv[]) {
+  int i;
+
+  for (i = 1; i < argc; i++) {
+    parse_string(argv[i]);
+    if (i < argc - 1) {
+      printf(" ");
+    }
+  }
+
+  printf("\n");
+}
