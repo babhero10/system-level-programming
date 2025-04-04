d8e09f50cbe9164947e98de95ce7f436fb5a4ff0
Abdullah Elsayed <abdallah.elsaid25@gmail.com>
2025-04-04 06:31:54 +0200

    FIX: EOF problem in femto_shell

--- shells/femto_shell.c
+++ shells/femto_shell.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
 void get_args(int *argc, char **argv[], char *text);
 void echo(int argc, char *argv[]);
@@ -12,19 +13,24 @@ int main(void) {
   char **my_argv = NULL;
   char *buffer = NULL;
   size_t len;
-  ssize_t read;
+  ssize_t ret;
 
   while (1) {
     printf("Femto shell prompt > ");
 
-    read = getline(&buffer, &len, stdin);
-    if (read == -1) {
-      perror("Error occurred while reading input.\n");
-      free(buffer);
-      exit(-1);
+    ret = getline(&buffer, &len, stdin);
+    if (ret == -1) {
+      if (errno != 0) {
+        fprintf(stderr, "Error occurred while reading input: %s.\n", strerror(errno));
+        free(buffer);
+        exit(-1);
+      } else {
+        free(buffer);
+        return 0;
+      }
+    } else {
+      buffer[strlen(buffer) - 1] = '\0';
     }
-
-    buffer[read - 1] = '\0';
 
     get_args(&my_argc, &my_argv, buffer);
 
@@ -32,12 +38,12 @@ int main(void) {
       if (strcmp(my_argv[0], "echo") == 0) {
         echo(my_argc, my_argv);
       } else if (strcmp(my_argv[0], "exit") == 0) {
-        printf("Good Bye!\n");
+        printf("Good Bye! ;)\n");
         break;
       } else if (strcmp(my_argv[0], "clear") == 0) {
         printf("\033[H\033[J");
       } else {
-        printf("Invalid command\n");
+        printf("Invalid command: %s\n", my_argv[0]);
       }
     }
 
@@ -47,6 +53,8 @@ int main(void) {
     buffer = NULL;
   }
 
+  free(my_argv);
+  free(buffer);
   return 0;
 }
 
